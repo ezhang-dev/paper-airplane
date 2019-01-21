1f5226d103b37c5eb4567c26df6a0598e1e45b6f
ezhang <74750261+ezhang-dev@users.noreply.github.com>
2019-01-20 20:42:01 -0500

    Implement file upload via put

--- db.js
+++ db.js
@@ -8,10 +8,11 @@ async function get(id){
 
 async function add(data){
   let id = await randomNumber(1, 9999);
+  if (db[id]) return add(data)
+  data.id=id
   db[id]=data
   console.log("Store",id,data)
   return id
 }
 
 module.exports={get,add}
-add({a:1}).then(q=>console.log(q,db))
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,36 @@
 var express = require('express');
 var db = require('../db.js');
+var fs = require('fs');
+var path = require('path');
+
+var repl = require('repl')
+
 var router = express.Router();
+
+function generatePath(id){
+	return path.join("blob",id.toString())
+}
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
    res.render('index', { title: 'Express' });
 });
-router.put('/upload',function(req, res, next) {
-  if (req.headers.expect=="")
-  console.log(req.rawBody);
-  next();
+router.put('/upload',async function(req, res, next) {
+	global.req=req
+	repl.start({useGlobal : true})
+	console.dir(req)
+	let data = {
+		'time': new Date(),
+		'type': "file",
+		'name': "file"
+	};
+	let id = await db.add(data)
+    req.pipe(fs.createWriteStream(generatePath(id),{flags:"w+"}))
+    .on('error', (err) => next(err))
+    .on('close', () => {
+      res.send(id.toString());
+    })
 });
 router.param('id', async function(req, res, next, id) {
   var data = await db.get(id);
